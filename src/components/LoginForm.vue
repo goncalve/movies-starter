e0cd9aa91939641848c3a3954db78cca7b2be2ef
GONCALVES ISABELLE <isabelle.goncalves@univ-lyon1.fr>
2024-05-28 21:32:47 +0200

    Initial commit

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="login-form">
+
+      <h1>Authentification</h1>
+      <p>Remplissez ce formulaire pour vous connecter.</p>
+
+      <label for="email">Email</label>
+      <input
+        type="email"
+        placeholder="Entrez votre courriel"
+        id="email"
+        name="email"
+        required
+      />
+
+      <label for="psw">Mot de passe</label>
+      <input
+        type="password"
+        placeholder="Entrez votre mot de passe"
+        id="psw"
+        name="psw"
+        required
+      />
+
+  </div>
+</template>
